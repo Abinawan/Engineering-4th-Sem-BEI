f4f47d0595e2a7b1736af621ae756a5ada163a03
Aaryab1 <88551900+Aaryab1@users.noreply.github.com>
2022-12-21 08:16:12 +0545

    Create linear_queue.c

---
+++ Data Structure and Algorithm/linear_queue.c
@@ -0,0 +1,85 @@
+//linear queue
+#include<stdio.h>
+#include<stdlib.h>
+#define size 50
+int front = 0;
+int rear = -1;
+int queue[size];
+void insertion();
+void deletes();
+void displays();
+int main()
+{
+    int choice;
+    
+    while(1)
+    {
+    printf("\nEnter your choice: ");
+    printf("0.Insert   1.Delete   2.Display    3.Exit\n");
+    scanf("%d",&choice);
+	
+    switch(choice)
+    {
+        case 0:
+            insertion();
+            break;
+        case 1:
+            deletes();
+            break;
+        case 2:
+            displays();
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("Please input validate choice");
+            break;
+
+    }
+    }
+}
+void insertion()
+{
+    if(rear == size -1)
+    {
+        printf("The queue is full");
+    }
+    else
+    {
+        int value;
+        printf("Enter the value: ");
+        scanf("%d",&value);
+        rear++;
+        queue[rear] = value;
+    }
+}
+void deletes()
+{
+    if(front > rear)
+    {
+        printf("The queue is empty");
+    }
+    else
+    {
+        printf("The deleted element is %d",queue[front]);
+        front++;
+    }
+}
+
+void displays()
+{
+    if(front > rear)
+    {
+        printf("The queue is empty");
+    }
+    else
+    {
+    	int i;
+        printf("The elemet in queue are:\n");
+        for( i = front; i <= rear; i++)
+        {
+            printf("%d",queue[i]);
+        }
+    }
+}
